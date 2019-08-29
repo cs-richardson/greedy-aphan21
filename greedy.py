add5cb697761f895c4a177ca90bef6fafdad7e20
Ann <aphan21@ssis.edu.vn>
2019-08-30 00:01:24 +0700

    Create greedy.py

---
+++ greedy.py
@@ -0,0 +1,59 @@
+"""
+This program is to convert the change owed to number of quarters, dimes, nickels and pennies.
+Coded by Ann Phan, help from stackoverflow.com to code for the check if an input is a positive float.
+"""
+while True:
+    try:
+        change_owed = float(input("How much change is owed?"))
+        if change_owed < 0: 
+            print("Sorry, positive floats only.")
+            continue
+        break
+    except ValueError:
+        print("Sorry, numbers only.")
+
+total_coins = int (change_owed * 100)
+
+quarters = int(total_coins / 25)
+if quarters == 1:
+    print("You need 1 quarter.")
+elif quarters > 1:
+    print("You need", quarters, "quarters.")
+not_quarters = int(total_coins % 25)
+if not_quarters == 0:
+    print("You need", quarters, "quarters.")
+
+dimes = int(not_quarters / 10)
+if dimes == 1:
+    print("You need 1 dime.")
+elif dimes > 1:
+    print("You need", dimes, "dimes.")
+not_dimes = int(not_quarters % 10)
+if not_dimes == 0:
+    print("You need", dimes, "dimes.")
+    
+nickels = int(not_dimes / 5)
+if nickels == 1:
+    print("You need 1 nickel.")
+elif nickels > 1:
+    print("You need", nickels, "nickels.")
+not_nickels = int(not_dimes % 5)
+if not_nickels == 0:
+    print("You need", nickels, "nickels.")
+
+pennies = int(not_nickels)
+if pennies == 1:
+    print("You need 1 penny.")
+elif pennies > 1:
+    print("You need", pennies, "pennies.")
+
+
+        
+
+
+
+
+
+
+
+
